30fd8939e8bd7f56ac8037d0397844dc07f1df88
George Deng <george@greenlite.io>
2023-10-09 12:46:35 -0700

    add example page

--- apps/docs/stories/pages/Example/Example.stories.ts
+++ apps/docs/stories/pages/Example/Example.stories.ts
@@ -1,14 +1,14 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { Page } from "./Page";
+import { ExamplePage } from ".";
 
 const meta = {
   title: "Example/Page",
-  component: Page,
+  component: ExamplePage,
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
     layout: "fullscreen",
   },
-} satisfies Meta<typeof Page>;
+} satisfies Meta<typeof ExamplePage>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
---
+++ apps/docs/stories/pages/Example/index.tsx
@@ -0,0 +1,81 @@
+import {
+  Anchor,
+  AppShell,
+  Burger,
+  Group,
+  List,
+  Text,
+  Title,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+
+export const ExamplePage = () => {
+  const [opened, { toggle }] = useDisclosure();
+
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{ width: 300, breakpoint: "sm", collapsed: { mobile: !opened } }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+        <div>Logo</div>
+      </AppShell.Header>
+      <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+
+      <AppShell.Main>
+        <Title order={2} mb={2}>
+          Pages in Storybook
+        </Title>
+        <Text>
+          We recommend building UIs with a{" "}
+          <Anchor
+            href="https://componentdriven.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Text fw={700} span>
+              component-driven
+            </Text>
+          </Anchor>{" "}
+          process starting with atomic components and ending with pages.
+        </Text>
+        <Text>
+          Render pages with mock data. This makes it easy to build and review
+          page states without needing to navigate to them in your app. Here are
+          some handy patterns for managing page data in Storybook:
+        </Text>
+        <List>
+          <List.Item>
+            Use a higher-level connected component. Storybook helps you compose
+            such data from the "args" of child component stories
+          </List.Item>
+          <List.Item>
+            Assemble data in the page component from your services. You can mock
+            these services out using Storybook.
+          </List.Item>
+        </List>
+        <Text>
+          Get a guided tutorial on component-driven development at{" "}
+          <Anchor
+            href="https://storybook.js.org/tutorials/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Storybook tutorials
+          </Anchor>
+          . Read more in the{" "}
+          <Anchor
+            href="https://storybook.js.org/docs"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            docs
+          </Anchor>
+          .
+        </Text>
+      </AppShell.Main>
+    </AppShell>
+  );
+};
